9760c06c1d876917b900f8e6e5524b2f0b78a3e2
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-08-30 17:37:21 +0200

    added find product by id to routes and to the service file

--- vite-project/serverRest/routes/productRoute.js
+++ vite-project/serverRest/routes/productRoute.js
@@ -6,6 +6,28 @@ const {
   findProductById,
 } = require("../db/productService");
 
+// GET all products
+
+// GET products by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      res.status(400).send("ID is required");
+      return;
+    }
+
+    const product = await findProductById(id);
+    if (!product) {
+      res.status(404).send("Product not found");
+      return;
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 // POST
 router.post("/", async (request, response) => {
   const product = request.body;
@@ -13,7 +35,7 @@ router.post("/", async (request, response) => {
   response.status(201).json(newProduct);
 });
 
-//DELETE by ID
+//DELETE product by ID
 router.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
