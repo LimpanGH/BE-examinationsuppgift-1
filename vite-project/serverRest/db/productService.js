efd919d4522e703f22611eeac65e1959fd9199b5
LimpanGH <Linusj555@gmail.com>
2024-08-31 09:34:42 +0200

    minor corrections  after a rebase

--- vite-project/serverRest/db/productService.js
+++ vite-project/serverRest/db/productService.js
@@ -1,29 +1,14 @@
-const ProductModel = require("./models/productModel");
+const ProductModel = require('./models/productModel');
 
 const createProduct = async (product) => {
   const newProduct = new ProductModel(product);
   return newProduct.save();
 };
 
-<<<<<<< HEAD
-const deleteProduct = async (id) => {
-  return ProductModel.findByIdAndDelete(id);
-};
-
-const findProductById = async (id) => {
-  return ProductModel.findById(id);
-};
-
-module.exports = { createProduct, deleteProduct, findProductById };
-=======
 const getProducts = async (product) => {
   return ProductModel.find();
 };
 
-<<<<<<< HEAD
-module.exports = { createProduct, getProducts };
->>>>>>> 56610b6 (organized server setup in index.js and added GET method in productService.js aswell as in productRoute.js)
-=======
 const deleteProduct = async (id) => {
   return ProductModel.findByIdAndDelete(id);
 };
@@ -33,4 +18,3 @@ const findProductById = async (id) => {
 };
 
 module.exports = { createProduct, getProducts, deleteProduct, findProductById };
->>>>>>> 95b04d4 (readded Anders changes and added functionality to search by Id in browser)
