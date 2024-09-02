a76767afade2af49e376691149074f20e142b2b5
LimpanGH <Linusj555@gmail.com>
2024-09-02 13:18:33 +0200

    readded routers after a merge removed them

--- vite-project/serverRest/routes/productRoute.js
+++ vite-project/serverRest/routes/productRoute.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   createProduct,
@@ -9,64 +9,75 @@ const {
   getTotalStockValue,
   getStockValueByManufacturerId,
   getManufacturers,
-} = require("../../serverRest/db/productService");
+  getCriticalStock,
+  getLowStock,
+} = require('../../serverRest/db/productService');
 
-router.get("/manufacturers", async (req, res) => {
+
+// Get /manufacturers
+router.get('/manufacturers', async (req, res) => {
   try {
     const manufacturers = await getManufacturers();
     res.status(200).json(manufacturers);
   } catch (error) {
-    console.error("Error getting manufacturers", error);
-    res.status(500).json({ error: "Failed to get manufacturers" });
+    console.error('Error getting manufacturers', error);
+    res.status(500).json({ error: 'Failed to get manufacturers' });
   }
 });
 
-router.get("/stock-value/:manufacturerId", async (req, res) => {
-  console.log("GET /api/products/stock-value/:manufacturerId");
+// GET /stock-value/:manufacturerId
+router.get('/stock-value/:manufacturerId', async (req, res) => {
+  console.log('GET /api/products/stock-value/:manufacturerId');
   try {
     const manufacturerId = req.params.manufacturerId;
     if (!manufacturerId) {
-      res.status(400).send("ManufacturerId is required");
+      res.status(400).send('ManufacturerId is required');
       return;
     }
-
     const stockValue = await getStockValueByManufacturerId(manufacturerId);
     res.status(200).json({ stockValue });
   } catch (error) {
-    console.error("Error getting stock value by manufacturerId", error);
-    res
-      .status(500)
-      .json({ error: "Failed to get stock value by manufacturerId" });
+    console.error('Error getting stock value by manufacturerId', error);
+    res.status(500).json({ error: 'Failed to get stock value by manufacturerId' });
   }
 });
 
 //GET  total stock value: //api/products/total-stock-value
-router.get("/total-stock-value", async (req, res) => {
-  console.log("GET /api/products/total-stock-value");
+router.get('/total-stock-value', async (req, res) => {
+  console.log('GET /api/products/total-stock-value');
   try {
     const totalStockValue = await getTotalStockValue();
     res.status(200).json({ totalStockValue });
   } catch (error) {
-    console.error("Error getting total stock value", error);
-    res.status(500).json({ error: "Failed to get total stock value" });
+    console.error('Error getting total stock value', error);
+    res.status(500).json({ error: 'Failed to get total stock value' });
   }
 });
 
+// GET /products/critical-stock
+router.get('/:id', async (req, res) => {
+  try {
+    const criticalStock = await getCriticalStock();
+    res.status(200).json(criticalStock);
+  } catch (error) {
+    console.error('Error getting products', error);
+    res.status(500).json({ error: 'Failed to get product' });
+  }
+});
 
-
-// GET product by ID
-router.get("/:id", async (req, res) => {
-  console.log("GET /api/products/:id");
+// GET products by ID
+router.get('/:id', async (req, res) => {
+  console.log('GET /api/products/:id');
   try {
     const id = req.params.id;
     if (!id) {
-      res.status(400).send("ID is required");
+      res.status(400).send('ID is required');
       return;
     }
 
     const product = await findProductById(id);
     if (!product) {
-      res.status(404).send("Product not found");
+      res.status(404).send('Product not found');
       return;
     }
     res.status(200).json(product);
@@ -75,47 +86,47 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// GET all products
-router.get("/", async (req, res) => {
-  try {
-    const products = await getProducts();
-    res.status(200).json(products);
-  } catch (error) {
-    console.error("Error getting products", error);
-    res.status(500).json({ error: "Failed to get product" });
-  }
-});
-
 // GET /products/low-stock
-router.get("/low-stock", async (req, res) => {
+router.get('/low-stock', async (req, res) => {
   try {
     const lowStock = await getLowStock();
     res.status(200).json(lowStock);
   } catch (error) {
-    console.error("Error getting products", error);
-    res.status(500).json({ error: "Failed to get product" });
+    console.error('Error getting products', error);
+    res.status(500).json({ error: 'Failed to get product' });
+  }
+});
+
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await getProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error('Error getting products', error);
+    res.status(500).json({ error: 'Failed to get product' });
   }
 });
 
 // POST
-router.post("/", async (request, response) => {
+router.post('/', async (request, response) => {
   const product = request.body;
   const newProduct = await createProduct(product);
   response.status(201).json(newProduct);
 });
 
 //PUT - update product by ID
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const id = req.params.id;
     if (!id) {
-      res.status(400).send("ID is required");
+      res.status(400).send('ID is required');
       return;
     }
 
     const product = await findProductById(id);
     if (!product) {
-      res.status(404).send("Product not found");
+      res.status(404).send('Product not found');
       return;
     }
 
@@ -128,18 +139,18 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE product by ID
-router.delete("/:id", async (req, res) => {
-  console.log("DELETE /api/products/:id");
+router.delete('/:id', async (req, res) => {
+  console.log('DELETE /api/products/:id');
   try {
     const id = req.params.id;
     if (!id) {
-      res.status(400).send("ID is required");
+      res.status(400).send('ID is required');
       return;
     }
 
     const product = await findProductById(id);
     if (!product) {
-      res.status(404).send("Product not found");
+      res.status(404).send('Product not found');
       return;
     }
     await deleteProduct(id);
