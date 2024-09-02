e2d92804822302bfdb1439b423f38f7396a56ea7
LimpanGH <Linusj555@gmail.com>
2024-09-02 10:45:15 +0200

    added getLowStock and getCriticalStock on the server-side

--- vite-project/serverRest/db/productService.js
+++ vite-project/serverRest/db/productService.js
@@ -9,6 +9,18 @@ const getProducts = async (product) => {
   return ProductModel.find();
 };
 
+const getLowStock = async () => {
+  const products = await ProductModel.find({ amountInStock: { $lt: 10 } }).exec();
+  console.log('Low stock products:', products);
+  return products;
+};
+
+const getCriticalStock = async () => {
+  const products = await ProductModel.find({ amountInStock: { $lt: 3 } }).exec();
+  console.log('Critical stock products:', products);
+  return products;
+};
+
 const deleteProduct = async (id) => {
   return ProductModel.findByIdAndDelete(id);
 };
@@ -17,4 +29,11 @@ const findProductById = async (id) => {
   return ProductModel.findById(id);
 };
 
-module.exports = { createProduct, getProducts, deleteProduct, findProductById };
+module.exports = {
+  createProduct,
+  getProducts,
+  getLowStock,
+  getCriticalStock,
+  deleteProduct,
+  findProductById,
+};
--- vite-project/serverRest/routes/productRoute.js
+++ vite-project/serverRest/routes/productRoute.js
@@ -1,39 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, deleteProduct,
-  findProductById,getProducts } = require('../../serverRest/db/productService');
+const {
+  createProduct,
+  deleteProduct,
+  findProductById,
+  getProducts,
+  getCriticalStock,
+  getLowStock,
+} = require('../../serverRest/db/productService');
 
-// GET all products
-
-// GET products by ID
-router.get("/:id", async (req, res) => {
+// GET /products/critical-stock
+router.get('/critical-stock', async (req, res) => {
   try {
-    const id = req.params.id;
-    if (!id) {
-      res.status(400).send("ID is required");
-      return;
-    }
-
-    const product = await findProductById(id);
-    if (!product) {
-      res.status(404).send("Product not found");
-      return;
-    }
-    res.status(200).json(product);
+    const criticalStock = await getCriticalStock();
+    res.status(200).json(criticalStock);
   } catch (error) {
-    res.status(500).send(error.message);
+    console.error('Error getting products', error);
+    res.status(500).json({ error: 'Failed to get product' });
   }
 });
 
-// POST
-router.post("/", async (request, response) => {
-  const product = request.body;
-  const newProduct = await createProduct(product);
-  response.status(201).json(newProduct);
+// GET /products/low-stock
+router.get('/low-stock', async (req, res) => {
+  try {
+    const lowStock = await getLowStock();
+    res.status(200).json(lowStock);
+  } catch (error) {
+    console.error('Error getting products', error);
+    res.status(500).json({ error: 'Failed to get product' });
+  }
 });
 
-// GET
-
+// GET all products
 router.get('/', async (req, res) => {
   try {
     const products = await getProducts();
@@ -41,48 +39,6 @@ router.get('/', async (req, res) => {
   } catch (error) {
     console.error('Error getting products', error);
     res.status(500).json({ error: 'Failed to get product' });
-  }
-});
-
-//DELETE product by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.status(400).send("ID is required");
-      return;
-    }
-
-    const product = await findProductById(id);
-    if (!product) {
-      res.status(404).send("Product not found");
-      return;
-    }
-    await deleteProduct(id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-//DELETE product by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.status(400).send("ID is required");
-      return;
-    }
-
-    const product = await findProductById(id);
-    if (!product) {
-      res.status(404).send("Product not found");
-      return;
-    }
-    await deleteProduct(id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).send(error.message);
   }
 });
 
@@ -104,6 +60,13 @@ router.get('/:id', async (req, res) => {
   } catch (error) {
     res.status(500).send(error.message);
   }
+});
+
+// POST
+router.post('/', async (request, response) => {
+  const product = request.body;
+  const newProduct = await createProduct(product);
+  response.status(201).json(newProduct);
 });
 
 //DELETE product by ID
