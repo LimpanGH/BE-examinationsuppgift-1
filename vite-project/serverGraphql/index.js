cbe3258f7c0c9c3d56110b06f366a8166aa34adb
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-09-02 13:50:33 +0200

    started to add graphql code within the index file

--- vite-project/serverGraphql/index.js
+++ vite-project/serverGraphql/index.js
@@ -3,12 +3,33 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT || 4000;
 const uri = process.env.MONGODB_URI;
 
-app.use(express.json());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    Schema: buildSchema(`
+        type RootQuery {
+            }
+            type RootMutation {
+            }
+                schema {
+                query: Query
+                mutation: Mutation
+                }
+    `),
+    rootvalue: {},
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Running a GraphQl server on port http://localhost:${port}`);
